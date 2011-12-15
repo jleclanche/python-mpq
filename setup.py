1f14750bb2beecc770a75651f82a42b30bb2297b
Jerome Leclanche <jerome@leclan.ch>
2011-12-15 22:44:34 +0000

    Initial commit. Bindings with open and close support

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os.path
+from distutils.core import setup, Extension
+
+#README = open(os.path.join(os.path.dirname(__file__), "README.rst")).read()
+
+CLASSIFIERS = [
+	"Development Status :: 5 - Production/Stable",
+	"Intended Audience :: Developers",
+	"License :: OSI Approved :: MIT License",
+	"Programming Language :: Python",
+]
+
+module = Extension("storm", sources=["storm/stormmodule.cc"], language="c++", libraries=["StormLib"])
+
+setup(
+	name = "python-storm",
+	ext_modules = [module],
+	author = "Jerome Leclanche",
+	author_email = "adys.wh@gmail.com",
+	classifiers = CLASSIFIERS,
+	description = "Python bindings for StormLib",
+	download_url = "http://github.com/Adys/python-storm/tarball/master",
+	#long_description = README,
+	url = "http://github.com/Adys/python-storm",
+	version = "1.0",
+)
---
+++ storm/stormmodule.cc
@@ -0,0 +1,67 @@
+#include <Python.h>
+#define STORM_MODULE
+#include "stormmodule.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+static PyObject *StormError;
+
+static PyObject * Storm_SFileOpenArchive(PyObject *self, PyObject *args) {
+	HANDLE mpq = NULL;
+	char *name;
+	int priority = 0;
+	int flags = 0;
+	bool result;
+
+	if (!PyArg_ParseTuple(args, "sii:SFileOpenArchive", &name, &priority, &flags)) {
+		return NULL;
+	}
+	result = SFileOpenArchive(name, priority, MPQ_OPEN_READ_ONLY, &mpq);
+
+	if (!result) {
+		PyErr_SetString(StormError, "Error opening archive");
+		return NULL;
+	}
+
+	return Py_BuildValue("l", mpq);
+}
+
+static PyObject * Storm_SFileCloseArchive(PyObject *self, PyObject *args) {
+	HANDLE mpq = NULL;
+
+	bool result;
+	if (!PyArg_ParseTuple(args, "i:SFileCloseArchive", &mpq)) {
+		return NULL;
+	}
+	result = SFileCloseArchive(mpq);
+
+	if (!result) {
+		PyErr_SetString(StormError, "Error closing archive");
+		return NULL;
+	}
+
+	Py_RETURN_NONE;
+}
+
+static PyMethodDef StormMethods[] = {
+	{"SFileOpenArchive",  Storm_SFileOpenArchive, METH_VARARGS, "Open a MPQ archive."},
+	{"SFileCloseArchive",  Storm_SFileCloseArchive, METH_VARARGS, "Close a MPQ archive."},
+	{NULL, NULL, 0, NULL} /* Sentinel */
+};
+
+PyMODINIT_FUNC initstorm(void) {
+	PyObject *m;
+
+	m = Py_InitModule("storm", StormMethods);
+	if (m == NULL) return;
+
+	StormError = PyErr_NewException((char *)"storm.error", NULL, NULL);
+	Py_INCREF(StormError);
+	PyModule_AddObject(m, "error", StormError);
+}
+
+#ifdef __cplusplus
+}
+#endif
