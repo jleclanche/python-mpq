850bcead2d602c3b9fffbfe0fcdfeebadfe061ef
Skarn <skarnproject@gmail.com>
2018-10-22 23:00:12 +0300

    Fix building on Mac OS 10.14

--- setup.py
+++ setup.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python
+import platform
 from setuptools import Extension, setup
+
+
+extra_link_args = []
+extra_compile_args = []
+# XCode for macOS Mojave issue
+if platform.mac_ver()[0] == "10.14":
+	for flags in extra_link_args, extra_compile_args:
+		flags += ["-stdlib=libc++", "-mmacosx-version-min=10.9"]
 
 
 module = Extension(
@@ -7,6 +16,8 @@ module = Extension(
 	sources=["mpq/stormmodule.cc"],
 	language="c++",
 	libraries=["storm"],
+	extra_compile_args=extra_compile_args,
+	extra_link_args=extra_link_args,
 )
 
 setup(ext_modules=[module])
